4b3dff4b9edc7dfb8cb26b36cf72bc41ce40aeb3
Anh Dat <anh23dat.truong@gmail.com>
2025-05-15 11:47:01 +0700

    fixing UI

--- src/app/(app)/chef/page.tsx
+++ src/app/(app)/chef/page.tsx
@@ -1,7 +1,7 @@
 export default function ChefPage() {
     return (
         <div>
-            <h1 className="text-2xl font-bold mb-4">Đầu bếp</h1>
+            <h1 className="min-h-screen text-2xl font-bold mb-4">Đầu bếp</h1>
             <p>Đây là trang đầu bếp.</p>
         </div>
     );
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,8 @@
-// app/(auth)/layout.tsx
+import '@/styles/main.scss';
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="min-h-screen bg-gradient-to-br from-[#fff1be] via-[#f2a4c6] to-[#d779da] flex items-center justify-center">
+        <div className="bg-auth">
             {children}
         </div>
     );
